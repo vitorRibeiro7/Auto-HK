1d29621ec84a6a781a542a659c1a1a8fd23c5394
Vitor Ribeiro <vitorhugomelo5@gmail.com>
2023-02-01 16:56:12 -0300

    :rocket: main added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
-import Header from "./components/header";
+import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
   return (
-    <div className="min-h-screen bg-blue-600 w-full flex flex-col">
+    <div className="min-h-screen bg-blue-600 w-full flex flex-col ">
       <Header />
+      <Main />
     </div>
   );
 }

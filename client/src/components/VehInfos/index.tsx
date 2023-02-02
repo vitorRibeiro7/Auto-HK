9e63b97f68f5325473a7b15a17ed8d7aa7879942
Vitor Ribeiro <vitorhugomelo5@gmail.com>
2023-02-02 18:17:27 -0300

    :rocket: vehicle infos fixed

--- client/src/components/VehInfos/index.tsx
+++ client/src/components/VehInfos/index.tsx
@@ -1,9 +1,32 @@
+import { useState } from "react";
 
 function VehInfos({ vehInfo }: any) {
+
+    const [vehInfoItem] = vehInfo;
+
+    const showInfos = () => {
+        console.log(vehInfoItem)
+    }
+
     return (
-        <div className="flex flex-col gap-1 h-auto mb-4">
-            <h1>{vehInfo.name}</h1>
-            <p>{vehInfo.year}</p>
+        <div className="flex flex-col gap-[2vw] bg-slate-100 h-full min-w-full p-6">
+            {/* <button onClick={showInfos}>teste</button> */}
+            <div>
+                <p className="text-2xl font-bold text-[2vw] text-blue-600">{vehInfoItem.name}</p>
+            </div>
+            <div className="w-full flex flex-row justify-start flex-wrap">
+                <div>
+                    <p className="text-zinc-600 text-[1vw] font-semibold">Marca</p>
+                    <p className="text-zinc-500 text-[1vw]  ">{vehInfoItem.brand}</p>
+                </div>
+                <div className="ml-24">
+                    <p className="text-zinc-600 text-[1vw] font-semibold">Ano</p>
+                    <p className="text-zinc-500  text-[1vw]">{vehInfoItem.year}</p>
+                </div>
+            </div>
+            <div className="max-h-52">
+                <p className="text-[1vw] font-normal overflow-auto">{vehInfoItem.desc}</p>
+            </div>
         </div>
     )
 }

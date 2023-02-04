310509c93220757ff65e2a1d6e24f0c24e5f3404
Vitor Ribeiro <vitorhugomelo5@gmail.com>
2023-02-03 22:42:55 -0300

    :rocket: design fixed

--- client/src/components/VehInfos/index.tsx
+++ client/src/components/VehInfos/index.tsx
@@ -12,10 +12,10 @@ function VehInfos({ vehInfo }: any) {
         <div className="flex flex-col gap-[2vw] bg-slate-100 h-full min-w-full p-6">
             {/* <button onClick={showInfos}>teste</button> */}
             <div>
-                <p className="text-2xl font-bold text-[2vw] text-blue-600">{vehInfoItem.name}</p>
+                <p className="text-2xl font-bold text-[1.7vw] text-blue-600">{vehInfoItem.name}</p>
             </div>
             <div className="w-full flex flex-row justify-start flex-wrap">
-                <div>
+                <div className="w-[7vw]">
                     <p className="text-zinc-600 text-[1vw] font-semibold">Marca</p>
                     <p className="text-zinc-500 text-[1vw]  ">{vehInfoItem.brand}</p>
                 </div>

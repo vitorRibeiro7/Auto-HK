55ac87ecba12b918c5d8bf01f85be8f2b59a4493
Vitor Ribeiro <vitorhugomelo5@gmail.com>
2023-02-13 18:23:54 -0300

    :rocket: create vehicle register component

---
+++ client/src/components/CreateModal/index.tsx
@@ -0,0 +1,50 @@
+import Modal from "../Modal"
+import Form from "../Form";
+
+import { api } from "../../services/api";
+
+interface valuesType {
+    name: string;
+    brand: string;
+    year: number;
+    description: string;
+    sold: boolean;
+}
+
+function CreateModal({ isOpen, onClose }: any) {
+
+
+    const initialValues = {
+        name: "",
+        brand: "",
+        year: 1980,
+        description: "",
+        sold: false
+    }
+
+    const handleSubmit = async (values: valuesType) => {
+
+        console.log(values)
+
+        try {
+            await api.post("/vehicle", {
+                name: values.name,
+                brand: values.brand,
+                year: values.year,
+                desc: values.description,
+                sold: values.sold,
+            });
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    return (
+        <Modal isOpen={isOpen} onClose={onClose}>
+            <Form initialValues={initialValues} onSubmit={handleSubmit} onCancel={onClose} title="Adicionar Veiculo" flag={false} />
+        </Modal>
+    )
+}
+
+export default CreateModal;

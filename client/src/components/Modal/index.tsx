06c7def9a2fb10a63d4c5033a9f81d1f3aecb578
Vitor Ribeiro <vitorhugomelo5@gmail.com>
2023-02-13 18:24:15 -0300

    :rocket: update modal css settings

--- client/src/components/Modal/index.tsx
+++ client/src/components/Modal/index.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 function Modal({ children, isOpen, onClose }: Props) {
     return isOpen ? (
-        <div className="fixed top-0 left-0 w-auto h-auto z-50 flex items-center justify-center bg-gray-900 bg-opacity-75">
+        <div className="fixed top-0 left-0 w-full h-full z-50 flex items-center justify-center bg-gray-900 bg-opacity-75">
             <div className="w-11/12 md:w-3/4 lg:w-1/2 bg-white rounded-lg py-8 px-6 shadow-xl z-20">
                 <button
                     className="absolute top-0 right-0 mt-4 mr-6 text-3xl font-bold text-slate-300 hover:text-gray-700"
